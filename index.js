31d6120aeba87cd7f62b2385aa4568110e6fcbf1
Muhammad Haseeb IQBAL <155799767+HaseebIqbal1199@users.noreply.github.com>
2025-03-27 21:56:12 +0500

    update CORS configuration to allow requests from any origin

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 // Cors
 app.use(cors({
-  origin: 'https://queuebotapp.vercel.app', // Allow requests from your frontend
-  methods: 'GET,POST', // Specify allowed HTTP methods
-  allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
-}));
+  origin: '*'
+}))
 
 // History
 let History = [
